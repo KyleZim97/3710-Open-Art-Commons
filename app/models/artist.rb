b9cfa10f5a52042c3e125a33ba6f0c54802fe8da
Kyle Zimmerman <kzimme15@msudenver.edu>
2024-12-03 16:13:02 -0700

    Finished Testing with Rspec

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -14,5 +14,5 @@ class Artist < ApplicationRecord
   validates :username, presence: true
   validates :about_artist, presence: true
   validates :created, presence: true
-
+  validates :password_confirmation, presence: true, if: -> { password.present? }
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,15 +2,93 @@ require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
   let(:artist) { FactoryBot.build(:artist) }
-  context 'Should validate' do
+
+  # valid fields test
+  context 'Should Validate' do
     it 'with valid fields' do
       expect(artist).to be_valid
     end
   end
 
-  #context 'when email is not valid' do
-    #let(:student) { FactoryBot.build(:artist, email: nil) }
+  # non valid fields tests. expected failure/sad paths
+  context 'Should Not Validate' do
+    it 'without a valid email' do
+      artist.email = nil
+      expect(artist).not_to be_valid
+    end
 
-    #it 
-  #pending "add some examples to (or delete) #{__FILE__}"
+    # starting with a fresh artists after updating the email
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'without a valid username' do
+      artist.username = nil
+      expect(artist).not_to be_valid
+    end
+
+    # test for about_artist being nil
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'without a valid about_artist' do
+      artist.about_artist = nil
+      expect(artist).not_to be_valid
+    end
+
+    # test for password being nil
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'without a valid password' do
+      artist.password = nil
+      expect(artist).not_to be_valid
+    end
+
+    # test for password_confirmation being nil
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'without a valid password_confirmation' do
+      artist.password_confirmation = nil
+      expect(artist).not_to be_valid
+    end
+
+    # test for improper email
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'with an improper email' do
+      artist.email = "email"
+      expect(artist).not_to be_valid
+    end
+
+    # test for duplicate emails
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'with a duplicate email' do
+      FactoryBot.create(:artist, email: 'test@example.com')
+      artist.email = 'test@example.com'
+      expect(artist).not_to be_valid
+    end
+
+    # test for improper passwords
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'with a short password' do
+      artist.password = 'Pass'
+      artist.password_confirmation = 'Pass'
+      expect(artist).not_to be_valid
+    end
+  end
+
+  # testing art association with artist 1 to many
+  context "Should allow many associated art" do
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'can have many associated arts' do
+      artist.save
+      art1 = FactoryBot.create(:art, artist: artist)
+      art2 = FactoryBot.create(:art, artist: artist)
+
+      expect(artist.arts).to include(art1, art2)
+    end
+  end
+
+  # testing successful deletion of art when artist is destroyed
+  context "Should destroy all associated art when artist destroyed" do
+    let(:artist) { FactoryBot.build(:artist) }
+    it 'destroys art' do
+      artist.save
+      art1 = FactoryBot.create(:art, artist: artist)
+
+      expect { artist.destroy }.to change(Art, :count).by(-1)
+    end
+  end
 end
